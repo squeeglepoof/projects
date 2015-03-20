e9134cf6d6f04a09c6819260ae266224311a4559
rebhuhnc <carrie.rebhuhn@gmail.com>
2015-03-20 15:52:25 -0700

    Added different speeds and multimind approach
    "Weighted" and "Crossweighted" still must be added, but "Multimind"
    appears functional

--- IROS2015/IROS2015/IROS2015.cpp
+++ IROS2015/IROS2015/IROS2015.cpp
@@ -37,7 +37,7 @@
 void program(int calls){
 	srand(time(NULL));
 	ATFMSectorDomain* domain = new ATFMSectorDomain();
-	SimTypeNE sim(domain, SimTypeNE::MULTIMIND);
+	SimTypeNE sim(domain, MultiagentTypeNE::MULTIMIND);
 	sim.runExperiment();
 
 	sim.outputRewardLog("stat_results/reward-"+to_string(calls)+".txt");
