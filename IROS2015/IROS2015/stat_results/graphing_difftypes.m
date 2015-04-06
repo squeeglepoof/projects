138e96660118ae65bcc43efd4d1d7313f966dced
rebhuhnc <carrie.rebhuhn@gmail.com>
2015-04-06 16:03:27 -0700

    fixed pathtraces

--- IROS2015/IROS2015/stat_results/graphing_difftypes.m
+++ IROS2015/IROS2015/stat_results/graphing_difftypes.m
@@ -10,7 +10,7 @@ weighted = [];
 crossweighted = [];
 multimind = [];
 
-for i=1:n_runs
+for i=0:(n_runs-1)
    blind = [blind; load(strcat(strcat('blind_conflict-',int2str(i)),'.txt'))];
    weighted = [blind; load(strcat(strcat('weighted_conflict-',int2str(i)),'.txt'))];
    crossweighted = [blind; load(strcat(strcat('crossweighted_conflict-',int2str(i)),'.txt'))];
@@ -19,8 +19,15 @@ end
 
 %%
 
-plot(-moving_average(mean(blind_conflict),5),'c*-')
-plot(-moving_average(mean(weighted_conflict),5),'b')
-plot(-moving_average(mean(crossweighted_conflict),5),'k')
-plot(-moving_average(mean(multimind_conflict),5),'g')
+hold on
 
+plot(-moving_average(mean(blind),5),'c*-')
+plot(-moving_average(mean(weighted),5),'b')
+plot(-moving_average(mean(crossweighted),5),'k')
+plot(-moving_average(mean(multimind),5),'g')
+
+legend('blind','weighted','crossweighted','multimind');
+xlabel('epoch')
+ylabel('conflict count')
+
+title('Comparison of different methods for agent type conflict radius=1 and conflict radius = 5')
