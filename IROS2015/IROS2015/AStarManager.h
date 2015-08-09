4175b6b3c3fe15a15e935d3c9adb870384d2055d
rebhuhnc <carrie.rebhuhn@gmail.com>
2015-08-08 21:37:52 -0400

    Compiling version with D
    Compiles, has D added in. Not yet moved A* stuff.

--- IROS2015/IROS2015/AStarManager.h
+++ IROS2015/IROS2015/AStarManager.h
@@ -10,13 +10,14 @@
 class AStarManager
 {
 public:
+	
 	AStarManager(void);
 	~AStarManager(void);
-	vector<XY> agent_locs;
+	/*vector<XY> agent_locs;
 	vector<AStar_easy::edge> edges;
 	// vector<double> weights; // old
 	matrix2d weights; // [type][connection]
 	std::vector<AStar_easy*> Astar_highlevel;
-	grid_lookup m2astar;
+	grid_lookup m2astar;*/
 };
 
